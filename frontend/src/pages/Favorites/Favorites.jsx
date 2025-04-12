82112b3659f3abc26e03eb5569a37b0e58750c11
Namaswi24 <142531985+Namaswi24@users.noreply.github.com>
2025-04-12 18:26:30 +0530

    Update Favorites.jsx

---
+++ frontend/src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import ProblemCard from "../../components/ProblemCard/ProblemCard"; // if you're listing favourite problems
+import "./Favourites.css";
